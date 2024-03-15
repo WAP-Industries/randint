5e288e8cb0189d2f7be89f3c7f0caef94964b752
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2024-03-15 21:52:56 +0800

    Update randint.py

--- randint.py
+++ randint.py
@@ -1,1 +1,1 @@
-def randint(start, stop): global _l; _l = float(f'0.{abs(hash(str(globals().get("_l") or hash(id))))}'); return int(_l*(stop-start+1))+start
+randint = lambda start,stop: exec('''globals()["_l"] = float(f'0.{abs(hash(str(globals().get("_l") or hash(id))))}')''') or int(_l*(stop-start+1))+start
